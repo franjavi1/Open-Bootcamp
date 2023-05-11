08e38691948eb3ea493593e5ba58ffa2ea649985
franjavi1 <118500511+franjavi1@users.noreply.github.com>
2023-05-11 00:51:12 -0300

    Add files via upload

---
+++ Resolución de Ejercicios/Tema 1/src/com/tema1/MiClase.java
@@ -0,0 +1,48 @@
+package com.tema1;
+
+public class MiClase {
+    public static void main(String[] args) {
+        //1. numericos
+
+        byte variable1 = 5;
+        short variable2 = 10;
+        int variable3 = 30;
+        long variable4 = 100;
+
+        //1.2 decimales
+        float variable5 = 5.5f;
+        double variable6 = 10.5d;
+
+        //2.booleano
+
+        boolean variable7 = false;
+        boolean variable8 = true;
+
+        //3.texto
+
+        char variable9 = 'a';
+        String variable10 = "Lorem ipsum dolor sit amet, consectetur adipis";
+
+        System.out.println("Valor byte: "+ variable1);
+        System.out.println("Valor short: "+ variable2);
+        System.out.println("Valor int: "+ variable3);
+        System.out.println("Valor long: "+ variable4);
+        System.out.println("Valor float: "+variable5);
+        System.out.println("Valor double: "+ variable6);
+        System.out.println("Valor boolean false: "+ variable7);
+        System.out.println("Valor boolean true : "+ variable8);
+
+
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
